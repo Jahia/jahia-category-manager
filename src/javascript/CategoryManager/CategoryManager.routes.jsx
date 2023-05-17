01108af67561772ff16842bc51cf7d19fb2eb0f8
Thomas Draier <tdraier@jahia.com>
2023-05-17 16:49:25 +0200

    BACKLOG-20968: add info in primary nav

--- src/javascript/CategoryManager/CategoryManager.routes.jsx
+++ src/javascript/CategoryManager/CategoryManager.routes.jsx
@@ -3,7 +3,7 @@ import {registry} from '@jahia/ui-extender';
 import CategoryManager from './CategoryManager';
 
 export const registerRoute = () => {
-    registry.add('primary-nav-item', 'categorymanagerNavGroup', {
+    registry.add('primary-nav-item', 'category-manager', {
         targets: ['nav-root-top:4'],
         requiredPermission: 'categoryManager',
         requiredPermissionPath: '/sites/systemsite',
